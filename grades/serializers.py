c66f92539ba1c119393eebbe52c0cd18844e3ce2
student04 <abcd@gmail.com>
2025-01-22 14:54:02 +0100

    Pierwwzy commit

---
+++ grades/serializers.py
@@ -0,0 +1,19 @@
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, UserSerializer
+from rest_framework import serializers
+
+from grades.models import Grade
+
+class Gradeserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grade
+        fields = ['grade','id_user','created_at']
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'username', 'password']
+
+
+class CurrentUserSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        fields = ['id','username', 'password']
